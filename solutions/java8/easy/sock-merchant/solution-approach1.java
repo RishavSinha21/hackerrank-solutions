df79d5bf3a0302c7f7003827102f4978ef6d2510
RishavSinha21 <rishavvsinha21@gmail.com>
2026-08-09 17:08:40 +0530

    ✅ Sales by Match [java8] — approach1

---
+++ solutions/java8/easy/sock-merchant/solution-approach1.java
@@ -0,0 +1,69 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/sock-merchant/problem?isFullScreen=true
+// Problem     Sales by Match
+// Difficulty  Easy
+// Subdomain   Algorithms
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-09, 05:08 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+          HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+          int count=0;
+          for(Integer b: ar){
+          map.put(b,map.getOrDefault(b, 0)+1);
+          }
+          for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            count+=entry.getValue()/2;
+            
+          }
+          return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

36029e4eab7bff12bedbe2c02eee738f953751ec
RishavSinha21 <rishavvsinha21@gmail.com>
2026-08-09 18:42:17 +0530

    ✅ Breaking the Records [java8] — approach1

---
+++ solutions/java8/easy/breaking-best-and-worst-records/solution-approach1.java
@@ -0,0 +1,76 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
+// Problem     Breaking the Records
+// Difficulty  Easy
+// Subdomain   Algorithms
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-09, 06:42 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        Stack<Integer> stack=new Stack<>();
+        List<Integer> list=new ArrayList<>();
+        stack.push(scores.get(0));
+        for(Integer i:scores){
+            if(stack.peek()<i){
+                stack.push(i);
+            }
+        }
+        list.add(stack.size()-1);stack.removeAllElements();stack.push(scores.get(0));
+        for(Integer i:scores){
+            if(stack.peek()>i){
+                stack.push(i);
+            }
+        }list.add(stack.size()-1);
+        return list;
+    }
+     
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

9151dd826d76d4cccba82e6bb7ad5529fad094e4
RishavSinha21 <rishavvsinha21@gmail.com>
2026-08-09 23:52:40 +0530

    ✅ Number Line Jumps [java8] — approach1

---
+++ solutions/java8/easy/kangaroo/solution-approach1.java
@@ -0,0 +1,67 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/kangaroo/problem?isFullScreen=true
+// Problem     Number Line Jumps
+// Difficulty  Easy
+// Subdomain   Algorithms
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-09, 11:52 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+       
+       if(v1-v2==0){return x1-x2==0 ? "YES":"NO";}
+       return ((x2-x1)%(v1-v2)==0 && (x2-x1)/(v1-v2) >= 0)?"YES" :"NO";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

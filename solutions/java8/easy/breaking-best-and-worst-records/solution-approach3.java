aa9b0b6b7fb1f01d353d09abc4cebbecf6887616
RishavSinha21 <rishavvsinha21@gmail.com>
2026-08-09 18:57:17 +0530

    Add complexity comment and problem link
    Added a comment summarizing the solution's complexity and linked to the problem.

--- solutions/java8/easy/breaking-best-and-worst-records/solution-approach3.java
+++ solutions/java8/easy/breaking-best-and-worst-records/solution-approach3.java
@@ -1,3 +1,5 @@
+//***************************************************           Solution has O(1) size complexity and O(N) time complexity         ******************************************************************************
+
 // ──────────────────────────────────────────────────
 // Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
 // Problem     Breaking the Records

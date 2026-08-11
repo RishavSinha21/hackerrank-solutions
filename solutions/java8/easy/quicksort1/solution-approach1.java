29bad15ce871bf6f4dd2bc231ce8cb3e42456b31
RishavSinha21 <rishavvsinha21@gmail.com>
2026-08-11 12:53:01 +0530

    ✅ Quicksort 1 - Partition [java8] — approach1

---
+++ solutions/java8/easy/quicksort1/solution-approach1.java
@@ -0,0 +1,98 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/quicksort1/problem?isFullScreen=true
+// Problem     Quicksort 1 - Partition
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-11, 12:52 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'quickSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> quickSort(List<Integer> arr) {
+        int[] array=new int[arr.size()];
+           for(int i=0;i<arr.size();i++){
+            array[i]=arr.get(i);
+           }
+           arr.clear();
+           sort(array,0,array.length-1);
+           for(int j=0;j<array.length;j++){
+            arr.add(array[j]);
+           }
+           return arr;
+
+    }
+    public static int[] sort(int[] array,int low,int high){
+        if(low<high){
+            
+            int pivotindex=parts(array,low,high);
+            sort(array,low,pivotindex-1);
+            sort(array,pivotindex+1,high);
+        }
+        return array;
+    }
+    public static int parts(int[] array,int low,int high){
+        int pivot=array[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(array[j]<pivot){
+                i++;
+                int temp=array[j];
+                array[j]=array[i];
+                array[i]=temp;
+            }
+        }
+        int temp=array[high];
+        array[high]=array[i+1];
+        array[i+1]=temp;
+        return i+1;
+    }
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.quickSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

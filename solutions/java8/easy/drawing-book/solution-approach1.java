7cf5ad89a354bcc0973c2393fd53425b30c3938c
RishavSinha21 <rishavvsinha21@gmail.com>
2026-09-01 22:11:57 +0530

    ✅ Drawing Book  [java8] — approach1

--- solutions/java8/easy/drawing-book/solution-approach1.java
+++ solutions/java8/easy/drawing-book/solution-approach1.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java8
 // Status      Accepted
-// Submitted   2026-09-01, 09:58 p.m.
+// Submitted   2026-09-01, 10:11 p.m.
 // ──────────────────────────────────────────────────
 
 import java.io.*;

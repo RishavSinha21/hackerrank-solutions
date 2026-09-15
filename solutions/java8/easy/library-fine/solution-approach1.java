34b0e71419f05a6093fb3c79d336474476bbab3a
RishavSinha21 <rishavvsinha21@gmail.com>
2026-09-15 15:33:33 +0530

    ✅ Library Fine [java8] — approach1

---
+++ solutions/java8/easy/library-fine/solution-approach1.java
@@ -0,0 +1,69 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/library-fine/problem?isFullScreen=true
+// Problem     Library Fine
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-15, 03:33 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+      if(y1-y2>0){
+        return (y1-y2)*10000;
+      }
+      else if(m1-m2>0 && y1-y2==0){
+        return 500*(m1-m2);
+      }
+      else if(d1-d2>0 && m1-m2==0 && y1-y2==0){
+        return 15*(d1-d2);
+      }
+      else{return 0;}
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

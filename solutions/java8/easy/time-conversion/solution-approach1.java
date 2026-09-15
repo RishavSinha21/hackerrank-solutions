047ed933c545084aa907c47a23b13202cc72bbfa
RishavSinha21 <rishavvsinha21@gmail.com>
2026-09-15 17:53:04 +0530

    ✅ Time Conversion [java8] — approach1

--- solutions/java8/easy/time-conversion/solution-approach1.java
+++ solutions/java8/easy/time-conversion/solution-approach1.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java8
 // Status      Accepted
-// Submitted   2026-09-15, 05:51 p.m.
+// Submitted   2026-09-15, 05:53 p.m.
 // ──────────────────────────────────────────────────
 
 import java.io.*;
